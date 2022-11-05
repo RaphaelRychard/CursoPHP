e723646010ad5a0f92753448c50230b5f8a953cd
Raphael Rychard <67989564+RaphaelRychard@users.noreply.github.com>
2022-11-05 09:37:49 -0400

    add

--- Aulas/aula05/operadores-matematicos.php
+++ Aulas/aula05/operadores-matematicos.php
@@ -26,7 +26,7 @@
                     echo "<br>O valor de $v1 <sup>$v2</sup> é ". pow($v1, $v2);
                     echo "<br>A raiz de $v1 é: ". sqrt($v1);
                     echo "<br> O valor $v2 arredondado é ". round($v2);/* ceil(arronda para cima) / floor (arredonda para baixo)*/
-                    echo "<br>A parete intiera de $V2 é ". intval($v2);
+                    echo "<br>A parete intiera de $2 é ". intval($v2);
                     echo "<br> O valor de $v1 em moeda é R$". number_format ($v1, 2, ",", ".");
                 ?>
               
--- teste/teste.php
+++ teste/teste.php
@@ -1,9 +1,25 @@
 <?php
 
-    for ($c=1 ; $c <= 10; $c++) {
-        
-        echo "$c ->";
+    
+    function somar () {
+        $p = func_get_args();
+        $tot = func_num_args();
+
+        for ($c=0 ; $c <= $tot; $c++) {
+            
+            echo $p[$c]. " ";
+            $somado += $p[$c] ;   
+        }
+        echo $somado;
+
+        somar (2, 3, 4, 5);
+
+        somar (2, 3, 4, 5);
+
+        somar (2, 3, 4, 5);
 
     }
+        
 
+    
 ?>
