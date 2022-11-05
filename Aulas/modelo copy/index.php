51b683db526c8ef2fc54eb2ef64a13439b35b838
Raphael Rychard <67989564+RaphaelRychard@users.noreply.github.com>
2022-11-05 10:44:52 -0400

    uo

---
+++ Aulas/modelo copy/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>PHP</title>
+        <link rel="stylesheet" href="css/style.css">
+    </head>
+
+    <body>
+        <article>
+            <h1>Curso de PHP para Iniciantes - By Raphael Rychard</h1>
+            <section>
+                <img id="logo" src="img/logo-php.png" alt="logo do PHP">
+                <?php
+                    $idade = 19;
+                    $nome = "Raphael Rychard";
+                    echo "$nome tem $idade Anos!";
+                ?>
+                <!-- as aspas tem que ser duplas -->
+            </section>
+
+        </article>
+
+    </body>
+
+</html>
